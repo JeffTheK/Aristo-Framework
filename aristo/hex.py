a69405897567aae1491c34e161a27097803ddb63
JeffTheK <jeff144@protonmail.com>
2021-12-12 21:36:59 +0200

    Fix hep.py

--- aristo/hex.py
+++ aristo/hex.py
@@ -21,9 +21,9 @@ GR = '\033[37m' # gray
 T  = '\033[93m' # tan
 def encode1():
     Str = raw_input(''+T+'' + color.UNDERLINE + 'String to encode>' + color.END)
-    Str = Str.encode('hex','strict');
-    print ""+G+"Encoded: " + Str
+    Str = Str.encode('hex','strict')
+    print(""+G+"Encoded: " + Str)
 def decode1():
     Str = raw_input(''+T+'' + color.UNDERLINE + 'String to decode>' + color.END)
-    Str = Str.decode('hex','strict');
-    print ""+G+"Decoded String: " + Str
+    Str = Str.decode('hex','strict')
+    print(""+G+"Decoded String: " + Str)
