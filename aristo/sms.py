7c2bc98fada935170f9258c880681831ff689af4
JeffTheK <jeff144@protonmail.com>
2021-12-12 21:30:37 +0200

    Fix sms.py

--- aristo/sms.py
+++ aristo/sms.py
@@ -25,27 +25,27 @@ C  = '\033[36m' # cyan
 GR = '\033[37m' # gray
 T  = '\033[93m' # tan  
 def sms():
-        print ""+T+" "
-	print "Put the @ sign before the provider"
-        provider = raw_input(color.UNDERLINE + 'Enter cellular provider>' + color.END)
-        print ""+T+" "
-	phone_num = raw_input(color.UNDERLINE + 'Victims phone number>' + color.END) + provider
-        print ""+T+" "
-	gmail = raw_input(color.UNDERLINE + 'Your email>' + color.END)
-        print ""+T+" "
-	password = getpass(color.UNDERLINE + 'Password>' + color.END)
+    print(""+T+" ")
+    print("Put the @ sign before the provider")
+    provider = raw_input(color.UNDERLINE + 'Enter cellular provider>' + color.END)
+    print(""+T+" ")
+    phone_num = raw_input(color.UNDERLINE + 'Victims phone number>' + color.END) + provider
+    print(""+T+" ")
+    gmail = raw_input(color.UNDERLINE + 'Your email>' + color.END)
+    print(""+T+" ")
+    password = getpass(color.UNDERLINE + 'Password>' + color.END)
 
-        o = smtplib.SMTP("smtp.gmail.com:587")
-        o.starttls()
-        o.login(gmail, password)
+    o = smtplib.SMTP("smtp.gmail.com:587")
+    o.starttls()
+    o.login(gmail, password)
 
-	message = raw_input(color.UNDERLINE + 'Message>' + color.END)
-        print ""+T+" "
-	counter = input(color.UNDERLINE + 'How many times>' + color.END)
-        print ""+T+" "
-        spam_msg = "From: {} \r\nTo: {} \r\n\r\n {}".format(gmail, phone_num, message)
-	print (color.UNDERLINE + ''+G+'[*] Sending...' + color.END)
-        for i in range(counter):
-            o.sendmail(gmail, phone_num, spam_msg)
-        sleep(0.1)
-	print (color.UNDERLINE + ''+G+'[*] Successfully sent!' + color.END)
+    message = raw_input(color.UNDERLINE + 'Message>' + color.END)
+    print ""+T+" "
+    counter = input(color.UNDERLINE + 'How many times>' + color.END)
+    print ""+T+" "
+    spam_msg = "From: {} \r\nTo: {} \r\n\r\n {}".format(gmail, phone_num, message)
+    print (color.UNDERLINE + ''+G+'[*] Sending...' + color.END)
+    for i in range(counter):
+        o.sendmail(gmail, phone_num, spam_msg)
+    sleep(0.1)
+    print (color.UNDERLINE + ''+G+'[*] Successfully sent!' + color.END)
