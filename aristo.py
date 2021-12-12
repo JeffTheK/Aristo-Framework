5856d6bb8822e53516d297ed7f2c42d4a8b2b603
JeffTheK <jeff144@protonmail.com>
2021-12-12 20:59:59 +0200

    Fix and convert some code

--- aristo.py
+++ aristo.py
@@ -39,19 +39,19 @@ try:
     import argparse
     import google
 except ImportError as e:
-    print (color.UNDERLINE + "\033[91m" + "You don't have some modules installed! \nPlease run install.py to install this tool fully! " + color.END)
-    print "Error: {}".format(e)
-    print "Execute: pip install (module name)"
+    print(color.UNDERLINE + "\033[91m" + "You don't have some modules installed! \nPlease run install.py to install this tool fully! " + color.END)
+    print("Error: {}".format(e))
+    print("Execute: pip install (module name)")
     if (e) == "DependencyWarning":
-	os.system("sudo apt-get update")
-	os.system("apt-get remove python-pip")
-	os.system("easy_install pip")
-	os.system("sudo pip uninstall requests")
-	os.system("sudo pip install requests")
+        os.system("sudo apt-get update")
+        os.system("apt-get remove python-pip")
+        os.system("easy_install pip")
+        os.system("sudo pip uninstall requests")
+        os.system("sudo pip install requests")
     elif (e) == "Unable to locate package lib32ncurses5":
-	os.system("sudo apt-get update")
-	os.system("sudo apt-get install lib32ncurses5 lib32bz2-1.0")
-    sys.exit()
+        os.system("sudo apt-get update")
+        os.system("sudo apt-get install lib32ncurses5 lib32bz2-1.0")
+        sys.exit()
 ###############################
 class color:
    PURPLE = '\033[95m'
@@ -82,41 +82,41 @@ M = '\033[1;35;32m' # magenta
 ###############################
 os.system('clear')
 if str(platform.system()) != "Linux":
-	sys.exit(""+R+"[!] " + color.UNDERLINE + "\033[91m" + "You are not using a Linux Based OS! Linux is a must-have for this script!" + color.END)
+    sys.exit(""+R+"[!] " + color.UNDERLINE + "\033[91m" + "You are not using a Linux Based OS! Linux is a must-have for this script!" + color.END)
 if not os.geteuid() == 0:
     sys.exit(""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Must be run as root. :/" + color.END)
 if 'no' in open('agree.txt').read():# take out the aristo/
-    print color.BOLD + """
+    print(color.BOLD + """
 Note that Aristo is provided as is, and is a royalty free open-source application.
 
 Feel free to modify, use, change, market, do whatever you want with it as long as you give the appropriate credit where credit is due (which means giving the authors the credit they deserve for writing it).
 
 Also by using this tool, you should try to make this tool better, try to stay positive, try to help others, try to learn from one another, try stay out of drama, try offer free hugs when possible (and make sure recipient agrees to mutual hug), and try to do everything you can to be awesome.
 Aristo is designed purely for good and not evil. If you are planning on using this tool for malicious purposes that are not authorized by the company you are performing assessments for, you are violating the terms of service and license of this toolset. By hitting yes (only one time), you agree to the terms of service and that you will only use this tool for lawful purposes only.
-"""
+""")
     agree = raw_input(''+G+'' + color.UNDERLINE + 'Do you agree to these terms and conditions?>' + color.END)
     if agree == "yes":
-	print (''+G+'' + color.UNDERLINE + 'Thanks!' + color.END)
-	time.sleep(3)
-	FILE = open("agree.txt","w")# take out the aristo/
+        print (''+G+'' + color.UNDERLINE + 'Thanks!' + color.END)
+        time.sleep(3)
+        FILE = open("agree.txt","w")# take out the aristo/
         FILE.write('yes')
         FILE.close()
     elif agree == "y":
-	print (''+G+'' + color.UNDERLINE + 'Thanks!' + color.END)
-	time.sleep(3)
-	FILE = open("agree.txt","w")# take out the aristo/
+        print (''+G+'' + color.UNDERLINE + 'Thanks!' + color.END)
+        time.sleep(3)
+        FILE = open("agree.txt","w")# take out the aristo/
         FILE.write('yes')
         FILE.close()
     elif agree == "Yes":
-	print (''+G+'' + color.UNDERLINE + 'Thanks!' + color.END)
-	time.sleep(3)
-	FILE = open("agree.txt","w")# take out the aristo/
+        print (''+G+'' + color.UNDERLINE + 'Thanks!' + color.END)
+        time.sleep(3)
+        FILE = open("agree.txt","w")# take out the aristo/
         FILE.write('yes')
         FILE.close()
     else:
-	print (''+R+'' + color.UNDERLINE + '[!] You have to agree!' + color.END)
-	time.sleep(1)
-	sys.exit()
+        print (''+R+'' + color.UNDERLINE + '[!] You have to agree!' + color.END)
+        time.sleep(1)
+        sys.exit()
 os.system('clear')
 banner()
 #============================================================#
@@ -161,7 +161,7 @@ def aristomain():
             main = raw_input(''+G+'' + color.BOLD + color.UNDERLINE + 'Tri>' + color.END)
             if main in swear:
                 print(""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Watch your language!" + color.END)
-	    elif main in spell:
+        elif main in spell:
                 print(""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Do you know how to spell?!" + color.END)
             elif main == "joke":
                 joke()
@@ -184,135 +184,135 @@ def aristomain():
                 print ""+P+"contact "+W+"- contact me"
                 print ""+W+"+----------------------------+"
                 print ""+R+"website "+W+"- enter a website and get its ip"
-	        print ""+R+"clone"+W+" - clone a websites source "
-	        print ""+R+"whois"+W+" - whois a website"
-	        print ""+R+"web"+W+" - extract info from a website"
-	        print ""+R+"siteexists"+W+" - check if a site exists"
-	        print ""+R+"google"+W+" - find google results for a query"
+            print ""+R+"clone"+W+" - clone a websites source "
+            print ""+R+"whois"+W+" - whois a website"
+            print ""+R+"web"+W+" - extract info from a website"
+            print ""+R+"siteexists"+W+" - check if a site exists"
+            print ""+R+"google"+W+" - find google results for a query"
                 print ""+W+"+----------------------------+"
-	        print ""+G+"ip "+W+"- geolocate an ip"
+            print ""+G+"ip "+W+"- geolocate an ip"
                 print ""+W+"+----------------------------+"
-	        print ""+O+"xss "+W+"- simple check for a xss vulnerability"
-	        print ""+O+"sql "+W+"- basic check for a sql vulnerability"
-	        print ""+O+"admin "+W+"- scan a website for its admin panel"
+            print ""+O+"xss "+W+"- simple check for a xss vulnerability"
+            print ""+O+"sql "+W+"- basic check for a sql vulnerability"
+            print ""+O+"admin "+W+"- scan a website for its admin panel"
                 print ""+W+"+----------------------------+"
-	        print ""+T+"email "+W+"- bomb an email address"
-	        print ""+T+"spoof email "+W+"- spoof an email address"
-	        print ""+T+"anonymous "+W+"- send an anonymous email"
-	        print ""+T+"sms"+W+" - spam text messages "
+            print ""+T+"email "+W+"- bomb an email address"
+            print ""+T+"spoof email "+W+"- spoof an email address"
+            print ""+T+"anonymous "+W+"- send an anonymous email"
+            print ""+T+"sms"+W+" - spam text messages "
                 print ""+T+"twitter"+W+" - check the details of a twitter account"
                 print ""+W+"+----------------------------+"
-	        print color.CYAN + "craft"+W+" - generate useful scripts "
-	        print color.CYAN + "qr"+W+" - generate a qr code"
-	        print color.CYAN + "zip"+W+" - crack a password-protected zip file"
+            print color.CYAN + "craft"+W+" - generate useful scripts "
+            print color.CYAN + "qr"+W+" - generate a qr code"
+            print color.CYAN + "zip"+W+" - crack a password-protected zip file"
                 print ""+W+"+----------------------------+"
-	        print color.BLUE + "encode base64"+W+" - text to base64"
-	        print color.BLUE + "decode base64"+W+" - base64 to text"
-	        print color.BLUE + "encode hex"+W+" - text to hex"
-	        print color.BLUE + "decode hex"+W+" - hex to text"
+            print color.BLUE + "encode base64"+W+" - text to base64"
+            print color.BLUE + "decode base64"+W+" - base64 to text"
+            print color.BLUE + "encode hex"+W+" - text to hex"
+            print color.BLUE + "decode hex"+W+" - hex to text"
                 print ""+W+"+----------------------------+"
-    	    elif main == "sms":
-	        sms()
-	    elif main == "xss":
-	        xss()
-	    elif main == "sql":
-	        sql()
-	    elif main == "anonymous":
-	        anon()
-	    elif main == "encode base64":
-	        encode()
-	    elif main == "decode base64":
-	        decode()
-	    elif main == "email":
-	        smtp()
-	    elif main == "quote":
-	        quote()
-	    elif main == "spoof email":
-	        spoofemail()
-	    elif main == "zip":
-	        zipfile()
-	    elif main == "decode hex":
-	        decode1()
-	    elif main == "encode hex":
-	        encode1()
-	    elif main == "google":
-	        googleSearch()
-	    elif main == "web":
-	        web()
-	    elif main == "siteexists":
-	        siteexists()
-	    elif main == "qr":
-	        gen_qrcode()
-	    elif main == "twitter":
-	        twitter()
-	    elif main == "anonymous":
-	        anon()
-	    elif main == "contact":
-	        print(''+T+'' + color.UNDERLINE + 'Skype:'+W+'' + color.BOLD + ' toxicnull@gmail.com' + color.END)
-	        print(''+T+'' + color.UNDERLINE + 'Instagram:'+W+'' + color.BOLD + ' @_t0x1c - www.instagram.com/_t0x1c' + color.END)
-	        print(''+T+'' + color.UNDERLINE + 'Email me:'+W+'' + color.BOLD + ' toxicnull@gmail.com' + color.END)
-	        print(''+T+'' + color.UNDERLINE + 'XMPP:'+W+'' + color.BOLD + ' toxic-ig@exploit.im' + color.END)
-		print(''+T+'' + color.UNDERLINE + 'Twitter:'+W+'' + color.BOLD + ' @toxicnull' + color.END)
-	    elif main == "ping":
-		while True:
-	            hostname = raw_input(''+T+'' + color.UNDERLINE + 'Host>' + color.END)
-	            os.system("ping " + hostname)
-	    elif main == "craft":
-		while True:
-	            table()
-	    elif main == "whois":
-	        whoisweb()
-	    elif main == "admin":
-	        admin()
-	    elif main == "banner":
-	        os.system('clear')
-	        banner()
-	        banner1()
-	    elif main == "speak":
-		while True:
-	            speak = raw_input(''+T+'' + color.UNDERLINE + 'What to say>' + color.END)
-	            os.system('espeak "' + speak + '"')
-	    elif main == "clone":
-	        clone()
-	    elif main == "cd":
-	        try:
-	            path = raw_input(''+T+'' + color.UNDERLINE + 'Directory>' + color.END)
-	            os.chdir(path)
-	        except OSError:
-	            print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "That is not a directory!" + color.END)
-	    elif main == "tool":
-	        print(color.UNDERLINE + ''+C+'Version: ' + (VersionNum) + color.END)
-		print(color.UNDERLINE + ''+C+'34 options to choose from!' + color.END)
-	        print(color.UNDERLINE + ''+C+'Time spent on it: 76 hours - 14 minutes' + color.END)
-	        print(color.UNDERLINE + ''+C+'toxic is a sp00ky h4ck3r' + color.END)
-	    elif main == "website":
-		while True:
-	            a = raw_input(''+T+'' + color.UNDERLINE + 'Website>' + color.END)
-	            try:
-	                print socket.gethostbyname(a)
-	            except socket.gaierror:
-	                print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Apparently host is unknown! :/" + color.END)
-	    elif main == "ip":
-	        ip = raw_input(''+T+'' + color.UNDERLINE + 'IP>' + color.END)
-	        if ip is None or ip == "":
-	            sys.exit(""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Please enter an IP!" + color.END)
-	        reversed_dns = socket.getfqdn(ip)
-	        geoip = urllib.urlopen('http://api.hackertarget.com/geoip/?q='
+            elif main == "sms":
+            sms()
+        elif main == "xss":
+            xss()
+        elif main == "sql":
+            sql()
+        elif main == "anonymous":
+            anon()
+        elif main == "encode base64":
+            encode()
+        elif main == "decode base64":
+            decode()
+        elif main == "email":
+            smtp()
+        elif main == "quote":
+            quote()
+        elif main == "spoof email":
+            spoofemail()
+        elif main == "zip":
+            zipfile()
+        elif main == "decode hex":
+            decode1()
+        elif main == "encode hex":
+            encode1()
+        elif main == "google":
+            googleSearch()
+        elif main == "web":
+            web()
+        elif main == "siteexists":
+            siteexists()
+        elif main == "qr":
+            gen_qrcode()
+        elif main == "twitter":
+            twitter()
+        elif main == "anonymous":
+            anon()
+        elif main == "contact":
+            print(''+T+'' + color.UNDERLINE + 'Skype:'+W+'' + color.BOLD + ' toxicnull@gmail.com' + color.END)
+            print(''+T+'' + color.UNDERLINE + 'Instagram:'+W+'' + color.BOLD + ' @_t0x1c - www.instagram.com/_t0x1c' + color.END)
+            print(''+T+'' + color.UNDERLINE + 'Email me:'+W+'' + color.BOLD + ' toxicnull@gmail.com' + color.END)
+            print(''+T+'' + color.UNDERLINE + 'XMPP:'+W+'' + color.BOLD + ' toxic-ig@exploit.im' + color.END)
+        print(''+T+'' + color.UNDERLINE + 'Twitter:'+W+'' + color.BOLD + ' @toxicnull' + color.END)
+        elif main == "ping":
+        while True:
+                hostname = raw_input(''+T+'' + color.UNDERLINE + 'Host>' + color.END)
+                os.system("ping " + hostname)
+        elif main == "craft":
+        while True:
+                table()
+        elif main == "whois":
+            whoisweb()
+        elif main == "admin":
+            admin()
+        elif main == "banner":
+            os.system('clear')
+            banner()
+            banner1()
+        elif main == "speak":
+        while True:
+                speak = raw_input(''+T+'' + color.UNDERLINE + 'What to say>' + color.END)
+                os.system('espeak "' + speak + '"')
+        elif main == "clone":
+            clone()
+        elif main == "cd":
+            try:
+                path = raw_input(''+T+'' + color.UNDERLINE + 'Directory>' + color.END)
+                os.chdir(path)
+            except OSError:
+                print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "That is not a directory!" + color.END)
+        elif main == "tool":
+            print(color.UNDERLINE + ''+C+'Version: ' + (VersionNum) + color.END)
+        print(color.UNDERLINE + ''+C+'34 options to choose from!' + color.END)
+            print(color.UNDERLINE + ''+C+'Time spent on it: 76 hours - 14 minutes' + color.END)
+            print(color.UNDERLINE + ''+C+'toxic is a sp00ky h4ck3r' + color.END)
+        elif main == "website":
+        while True:
+                a = raw_input(''+T+'' + color.UNDERLINE + 'Website>' + color.END)
+                try:
+                    print socket.gethostbyname(a)
+                except socket.gaierror:
+                    print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Apparently host is unknown! :/" + color.END)
+        elif main == "ip":
+            ip = raw_input(''+T+'' + color.UNDERLINE + 'IP>' + color.END)
+            if ip is None or ip == "":
+                sys.exit(""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Please enter an IP!" + color.END)
+            reversed_dns = socket.getfqdn(ip)
+            geoip = urllib.urlopen('http://api.hackertarget.com/geoip/?q='
                                + ip).read().rstrip()
-	        print (""+G+"[*] " + color.UNDERLINE + "\033[91m" + "IP Info" + color.END)
-	        print geoip
-	    elif main == "clear":
-	        os.system('clear')
+            print (""+G+"[*] " + color.UNDERLINE + "\033[91m" + "IP Info" + color.END)
+            print geoip
+        elif main == "clear":
+            os.system('clear')
             elif main == "exit":
-	        print (""+G+"[*] " + color.UNDERLINE + "\033[91m" + "Exiting..." + color.END)
-	        print (""+G+"[*] " + color.UNDERLINE + "\033[92m" + "GoodBye!" + color.END)
-	        time.sleep(0.2)
-	        sys.exit()
-	    elif main == "":
-	        print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Please enter an option!" + color.END)
+            print (""+G+"[*] " + color.UNDERLINE + "\033[91m" + "Exiting..." + color.END)
+            print (""+G+"[*] " + color.UNDERLINE + "\033[92m" + "GoodBye!" + color.END)
+            time.sleep(0.2)
+            sys.exit()
+        elif main == "":
+            print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "Please enter an option!" + color.END)
             else:
-	        print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "That is not an option!" + color.END)
+            print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "That is not an option!" + color.END)
         except KeyboardInterrupt:
-		print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "\nCtrl-C Pressed! Use 'exit' to close the tool!" + color.END)
-		aristomain()
+        print (""+R+"[!] " + color.UNDERLINE + "\033[91m" + "\nCtrl-C Pressed! Use 'exit' to close the tool!" + color.END)
+        aristomain()
 aristomain()
