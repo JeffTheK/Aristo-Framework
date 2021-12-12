d57f05c324601f5e5c3f28ee4b2ec1f170cbd48c
JeffTheK <jeff144@protonmail.com>
2021-12-12 21:53:09 +0200

    Fix web.py

--- aristo/web.py
+++ aristo/web.py
@@ -30,17 +30,17 @@ def web():
     try:
         web = raw_input(''+G+'' + color.UNDERLINE + 'Website>' + color.END)
     except IOError:
-	print(''+G+'' + color.UNDERLINE + '[!] Host is in wrong format - e.g. http://example.com' + color.END)
+    print(''+G+'' + color.UNDERLINE + '[!] Host is in wrong format - e.g. http://example.com' + color.END)
     response = urllib.urlopen(web)
-    print 'RESPONSE:', response
-    print 'URL     :', response.geturl()
+    print('RESPONSE:', response)
+    print('URL     :', response.geturl())
 
     headers = response.info()
-    print 'DATE    :', headers['date']
-    print 'HEADERS :'
-    print '---------'
-    print headers
+    print('DATE    :', headers['date'])
+    print('HEADERS :')
+    print('---------')
+    print(headers)
 
     data = response.read()
-    print 'LENGTH  :', len(data)
-    print '---------'
+    print('LENGTH  :', len(data))
+    print('---------')
