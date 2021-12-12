cce0bef14cceb4b9bf351c463dad76c780bb7529
JeffTheK <jeff144@protonmail.com>
2021-12-12 21:50:28 +0200

    Fix print

--- aristo/qr.py
+++ aristo/qr.py
@@ -32,7 +32,7 @@ M = '\033[1;35;32m' # magenta
 
 def gen_qrcode():
     url = raw_input(''+T+'' + color.UNDERLINE + 'Website or text>' + color.END)
-    print ""+C+"Enter the name of the output file without the extension"
+    print(""+C+"Enter the name of the output file without the extension")
     name = raw_input(''+T+'' + color.UNDERLINE + 'Output>' + color.END)
     qr = qrcode.QRCode(5, error_correction=qrcode.constants.ERROR_CORRECT_L)
     qr.add_data(url)
